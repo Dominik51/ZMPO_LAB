f312d6276e3c06d664bdccdc392ed7bbb71c8a89
Dominik <45131197+Dominik51@users.noreply.github.com>
2018-11-30 11:08:55 +0100

    defined .h file

--- CIndividual.h
+++ CIndividual.h
@@ -5,7 +5,9 @@ using namespace std;
 class CIndividual
 {
 public:
-
+	int iGetAdaptation();
+	void vMutate();
+	CIndividual cCrossover(CIndividual c_individual);
 private:
-
+	int *genotype = nullptr;
 };
