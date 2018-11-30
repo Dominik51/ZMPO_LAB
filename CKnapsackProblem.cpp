8b1688551d2385b4e525d16a57bce52b3829134e
Dominik <45131197+Dominik51@users.noreply.github.com>
2018-11-30 12:07:05 +0100

    Add files via upload

--- CKnapsackProblem.cpp
+++ CKnapsackProblem.cpp
@@ -1,1 +1,45 @@
 #include "CKnapsackProblem.h"
+
+CKnapsackProblem::CKnapsackProblem()
+{
+}
+
+bool CKnapsackProblem::bSetDCapacity(double d_capacity)
+{
+	if (d_capacity > 0)
+	{
+		dCapacity = d_capacity;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool CKnapsackProblem::vSetVItemsSize(vector<double> *v_items_size)
+{
+	if (v_items_size != nullptr)
+	{
+		vItemsSize = v_items_size;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+}
+
+bool CKnapsackProblem::vSetVItemsValue(vector<double>* v_items_value)
+{
+	if (v_items_value != nullptr)
+	{
+		vItemsValue = v_items_value;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
--- CKnapsackProblem.h
+++ CKnapsackProblem.h
@@ -6,9 +6,12 @@ class CKnapsackProblem
 {
 public:
 	CKnapsackProblem();
+	bool bSetDCapacity(double d_capacity);
+	bool vSetVItemsSize(vector<double> *v_items_size);
+	bool vSetVItemsValue(vector<double> *v_items_value);
 
 private:
 	double dCapacity;
-	vector<double> vItemsSize;
-	vector<double> vItemsValue;
+	vector<double> *vItemsSize;
+	vector<double> *vItemsValue;
 };
